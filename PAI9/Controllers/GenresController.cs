30bf0bb252642ab0a3d4791268ed371d65b7b9b9
Marceli Jerzynski <marceli.jerzynski@franklintempleton.com>
2020-05-24 19:38:53 +0200

    Lab9 solved in this commit

---
+++ PAI9/Controllers/GenresController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PAI9.Models;
+
+namespace PAI9.Controllers
+{
+    public class GenresController : Controller
+    {
+        private DbMusicContext db = new DbMusicContext();
+
+        // GET: Genres
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        // GET: Genres/Details/5
+        public ActionResult Details(int? id)
+        {
+            var filteredSongs = new List<Song>();
+            foreach(var song in db.Songs.ToList())
+            {
+                if (song.GenreId == id.ToString())
+                {
+                    filteredSongs.Add(song);
+                }
+            }
+            ViewBag.FilteredSongs = filteredSongs;
+            ViewBag.Genres = db.Genres.ToList();
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+        // GET: Genres/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Genres/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Genre genre = db.Genres.Find(id);
+        //    if (genre == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(genre);
+        //}
+
+        // POST: Genres/Delete/5
+        [HttpDelete, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Genre genre = db.Genres.Find(id);
+            db.Genres.Remove(genre);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PAI9/Models/DbMusicContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PAI9.Models
+{
+    public class DbMusicContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
